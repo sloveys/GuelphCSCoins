c392a5ae082cc243c643ce637244a7d755809294
sloveys <sloveys@mail.uoguelph.ca>
2017-03-21 15:01:40 -0400

    mt random number gen figured out

--- nbproject/MinerMain.py
+++ nbproject/MinerMain.py
@@ -5,5 +5,17 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
+import random
+
+"returns random number from Mersenne Twister algorithm"
+def get_random_mt(seed = 0):
+    random.seed(seed)
+    rn = random.getrandbits(32)
+    return rn
+
+
 if __name__ == "__main__":
-    print "Hello World"
+    rn = get_random_mt(1)
+    print rn
+
+
