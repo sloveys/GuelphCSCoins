8272aa562f8acb3869d91670b38ddc32f3122d28
sloveys <sloveys@mail.uoguelph.ca>
2017-03-21 14:34:39 -0400

    Simulation of server added
    not sure if this is how the server input should be set up. this is a
    temparary interface

---
+++ nbproject/SimulationOfServer.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+def get_sorted_list_challenge ():
+    c_list = [0, "sorted_list", "0000000000000000000000000000000000000000000000000000000000000000", "94f9", 20]
+    return c_list
+    
